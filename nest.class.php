fc716372893c415337e6c49b55a46d75e972746f
Guillaume Boudreau <github@danslereseau.com>
2013-02-21 11:50:35 -0500

    Fixes #4 correctly! :)

--- nest.class.php
+++ nest.class.php
@@ -66,7 +66,7 @@ class Nest {
 		$manual_away = $this->last_status->structure->{$structure}->away;
 		$mode = strtolower($this->last_status->device->{$serial_number}->current_schedule_mode);
 		$target_mode = $this->last_status->shared->{$serial_number}->target_temperature_type;
-		if ($manual_away || $mode == 'away' || $this->last_status->shared->{$serial_number}->auto_away !== 0) {
+		if ($manual_away || $mode == 'away' || $this->last_status->shared->{$serial_number}->auto_away > 0) {
 			$mode = $mode . ',away';
 			$target_mode = 'range';
 			$target_temperatures = array($this->last_status->device->{$serial_number}->away_temperature_low, $this->last_status->device->{$serial_number}->away_temperature_high);
