7eb45f00a397452e5a7444ad6d6107996dfb29eb
bauzer714 <bauzer714@gmail.com>
2015-07-08 11:11:11 -0400

    Missed comma after constant name
    Missed a comma causing the constant to error.

--- nest.class.php
+++ nest.class.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DATE_FORMAT','Y-m-d');
-define('DATETIME_FORMAT' DATE_FORMAT . ' H:i:s');
+define('DATETIME_FORMAT', DATE_FORMAT . ' H:i:s');
 define('TARGET_TEMP_MODE_COOL', 'cool');
 define('TARGET_TEMP_MODE_HEAT', 'heat');
 define('TARGET_TEMP_MODE_RANGE', 'range');
